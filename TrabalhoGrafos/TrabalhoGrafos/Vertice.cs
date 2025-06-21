8852654c087d8ab681d619ec2e98ca8ac6df3200
samuelvaladaop25 <145077672+samuelvaladaop25@users.noreply.github.com>
2025-06-21 17:19:02 -0300

    Update Menu, Vertices e Arestas

--- TrabalhoGrafos/TrabalhoGrafos/Vertice.cs
+++ TrabalhoGrafos/TrabalhoGrafos/Vertice.cs
@@ -3,26 +3,41 @@ using TrabalhoGrafos;
 
 public class Vertice
 {
-    private List<Aresta> _arestas;
+    private List<Aresta> _arestasEntrantes;
+    private List<Aresta> _arestasSaintes;
     private string _nome;
 
     public Vertice(string nome)
     {
         _nome = nome;
-        _arestas = new List<Aresta>();
+        _arestasEntrantes = new List<Aresta>();
+        _arestasSaintes = new List<Aresta>();
     }
 
     public string Nome => _nome;
 
-    public bool AdicionarAresta(Vertice destino, int peso)
+    public bool AdicionarArestaEntrando(Vertice origem, int peso)
     {
-        Aresta novaAresta = new Aresta(this, destino, peso);
+        Aresta novaAresta = new Aresta(origem, this, peso);
 
-        if (_arestas.Any(a => a.Destino == destino && a.Peso == peso))
+        if (_arestasEntrantes.Any(a => a.Origem == origem && a.Peso == peso))
         {
             return false; // Aresta já existe
         }
-        _arestas.Add(novaAresta);
+        _arestasEntrantes.Add(novaAresta);
+
+        return true;
+    }
+
+    public bool AdicionarArestaSaindo(Vertice destino, int peso)
+    {
+        Aresta novaAresta = new Aresta(this, destino, peso);
+
+        if (_arestasSaintes.Any(a => a.Destino == destino && a.Peso == peso))
+        {
+            return false; // Aresta já existe
+        }
+        _arestasSaintes.Add(novaAresta);
 
         return true;
     }
@@ -31,7 +46,7 @@ public class Vertice
     {
         StringBuilder sb = new StringBuilder();
 
-        foreach (Aresta aresta in _arestas)
+        foreach (Aresta aresta in _arestasSaintes)
         {
             sb.AppendLine(aresta.ToString());
         }
